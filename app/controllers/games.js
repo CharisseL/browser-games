c19c497d5d2e3f9462e1d2feabf909938f8722ef
Charisse Lambert <charisse8@gmail.com>
2014-08-15 15:15:58 -0500

    pixel class

---
+++ app/controllers/games.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.colors = function(req, res){
+  res.render('games/colors');
+};
+
+exports.pacman = function(req, res){
+  res.render('games/pacman');
+};
+
+
+exports.painter = function(req, res){
+  res.render('games/painter');
+};
+
+
---
+++ app/static/js/user/pacman.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    alert('Pacman is ready to CHOMP');
+  });
+
+
+})();
+
